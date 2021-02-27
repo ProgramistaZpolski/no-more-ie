d67c6cdb38c7e974cdabf731d7aeee114c8b63b3
Piotr Badełek <piotrbadelek@protonmail.com>
2021-02-27 17:06:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+"use strict";
+if ((window.navigator.userAgent.indexOf('MSIE ') > 0) || (window.navigator.userAgent.indexOf('Trident/') > 0)) {
+	window.location = "https://www.mozilla.org/en-CA/firefox/new/";
+};
